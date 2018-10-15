585f88c34424ef1d2bdf415e443a93ee60f8f4ef
seemas <simonas@adapt.dk>
2018-10-15 10:24:24 +0300

    Pushed the code forward :)

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# ubuntu-jenkins-indicator
+# Ubuntu Jenkins Indicator
 Fetches JENKINS jobs via api and displays last job status in UBUNTU indication toolbar next to clock.
+
+# Run using command line 
+python jenkins-indicator.py
+
+# Config
+Edit `config.json` and add your data you want to monitor.
+
+
---
+++ jenkins-indicator.py
@@ -0,0 +1,89 @@
+import gi
+
+gi.require_version('Gtk', '3.0')
+gi.require_version('AppIndicator3', '0.1')
+from gi.repository import Gtk as gtk
+from gi.repository import AppIndicator3 as appindicator
+from gi.repository import GObject as gobject
+import signal
+import os
+import urllib
+import json
+
+APPINDICATOR_ID = "jenkins-indicator"
+CURRPATH = os.path.dirname(os.path.realpath(__file__))
+
+
+# Quit script
+def quit(source):
+    gtk.main_quit()
+
+
+##############################
+# Update Jenkins statuses
+##############################
+def jenkis_update():
+    file = open("config.json", "r")
+    data = json.loads(file.read())
+    host = data["host"]
+    string = ""
+    for name, job in data["jobs"].items():
+        url = "http://" + host + "/job/" + job + "/lastBuild/api/json?tree=building,result"
+        try:
+            response = urllib.urlopen(url)
+            data = json.loads(response.read())
+            result = str(data["result"])
+            if result == "None":
+                result = "BUILDING"
+            string = string + " " + name + ":" + result
+        except IOError:
+            string = "NO CONN..."
+    return string + " "
+
+
+# Update statuses
+def update_statuses(indicator):
+    jobStatus = solar_jenkis_update()
+    indicator.set_label(jobStatus, "")
+    return True
+
+
+##############################
+# Create menu
+##############################
+def build_menu():
+    menu = gtk.Menu()
+
+    # Quit
+    item_quit = gtk.MenuItem("Stop it")
+    item_quit.connect("activate", quit)
+    menu.append(item_quit)
+    menu.show_all()
+    return menu
+
+
+##############################
+# Main function
+##############################
+def main():
+    # Set up indicator
+    indicator = appindicator.Indicator.new(APPINDICATOR_ID,
+                                           os.path.abspath('fire.svg'),
+                                           appindicator.IndicatorCategory.SYSTEM_SERVICES)
+    indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
+    indicator.set_menu(build_menu())
+
+    indicator.set_label("LOADING...", "")
+
+    # Allow stop signal
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+    # Update statuses every 60 seconds
+    updatetimer = gobject.timeout_add_seconds(60, update_statuses, indicator)
+
+    # Start main loop
+    gtk.main()
+
+
+if __name__ == "__main__":
+    main()
